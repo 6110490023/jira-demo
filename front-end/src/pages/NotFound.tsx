e5a6278a312c55d8b1b10f01f26d7f0b4bc0d728
eknarin <eknarin@digitalcommerce.io>
2025-04-03 22:12:56 +0700

    [update]not-found

---
+++ front-end/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const NotFound: React.FC = () => {
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="w-full max-w-sm p-6 bg-white text-black rounded-lg shadow-lg">
+                Not Found
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -3,6 +3,7 @@ import { AlertProvider } from "./context/AlertContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NotFound from "./pages/Notfound";
 const AppRoutes = () => {
   return (
     <AlertProvider>
@@ -11,6 +12,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
     </AlertProvider>
