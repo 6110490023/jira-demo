76140817982db64bd9105138918749405b91b415
eknarin <eknarin@digitalcommerce.io>
2025-04-03 21:06:14 +0700

    [update]remove StrictMode

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000 // เปลี่ยนเป็นพอร์ตที่ต้องการ เช่น 3000
+    port: 3000 
   }
 })
