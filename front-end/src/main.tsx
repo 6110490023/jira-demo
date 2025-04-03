1914d94b54c4d94848e2e793e2694f4f504ef42c
eknarin <eknarin@digitalcommerce.io>
2025-04-03 21:59:10 +0700

    [update]add-stricMode

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,10 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import AppRoutes from "./routes";
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
+  <StrictMode>
     <AppRoutes />
-  // </StrictMode>,
+  </StrictMode>,
 )
